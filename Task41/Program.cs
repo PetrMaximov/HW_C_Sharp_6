080052911e474a43f5e44018215c74ba975fa1e6
«Petr» <maxpetrb@mail.ru>
2023-09-20 20:27:53 +0300

    Task41 сколько числе из введеных больше 0

---
+++ Task41/Program.cs
@@ -0,0 +1,21 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+int N = 0;
+Console.WriteLine("Введите числа через пробел");
+string[] num = Console.ReadLine().Split(' ');
+int M = num.Length;
+int[] A = new int[M];
+              
+for (int i = 0; i < M; i++) 
+{
+    A[i] = int.Parse(num[i]);
+    if (A[i] > 0) {
+        N++;
+        }
+}            
+        
+Console.WriteLine("Количество чисел больше 0: " + N);
+              
+              
+
+           
